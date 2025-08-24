30ce8b299d6e1e501966b6bf490db6a2e5ec6c6b
Vinicius Coretti Tegi <viniciustegi1235@gmail.com>
2025-08-24 15:28:41 -0300

    Aula001 - last commit

--- Cursos/Bootcamp Riachuelo/Aula001/src/Main.java
+++ Cursos/Bootcamp Riachuelo/Aula001/src/Main.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
+
+    private final static String WELCOME_MESSAGE = "Ola, informe o seu nome";
+
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+        System.out.println(WELCOME_MESSAGE);
+        var name = scanner.next();
+        System.out.println("Informe sua idade");
+        var age = scanner.nextInt();
+
+        System.out.printf("Ola %s, sua idade eh de %s", name, age);
+    }
 }

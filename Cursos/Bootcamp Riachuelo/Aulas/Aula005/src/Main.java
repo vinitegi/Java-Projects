c67c6981e57f9841e54fa233ec07df4e7e21fc84
Vinicius Coretti Tegi <viniciustegi1235@gmail.com>
2025-10-11 15:18:16 -0300

    Aula005 - last commit

--- Cursos/Bootcamp Riachuelo/Aulas/Aula005/src/Main.java
+++ Cursos/Bootcamp Riachuelo/Aulas/Aula005/src/Main.java
@@ -1,14 +1,17 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        var sc = new Scanner(System.in);
-        var male = new Person();
+        var person1 = new Person();
 
-        male.setName("Jhon");
-        male.setAge(19);
+        //valores iniciais, antes do modificador 'set'
+        System.out.println(person1.getName());
+        System.out.println(person1.getAge());
 
-        System.out.println(male.getName());
-        System.out.println(male.getAge());
+        //vai atribuir o valor aos metodos da instância da classe
+        person1.setName("Vinicius C. Tegi");
+        person1.setAge(19);
+
+        //vai retornar o valor dos metodos da instância da classe, atribuidos pelos metodos 'set'
+        System.out.println(person1.getName());
+        System.out.println(person1.getAge());
     }
 }
--- Cursos/Bootcamp Riachuelo/Aulas/Aula005/src/Person.java
+++ Cursos/Bootcamp Riachuelo/Aulas/Aula005/src/Person.java
@@ -1,18 +1,29 @@
 public class Person {
-    private String name;
-    private int age;
+    private String name = "Jhon";
+    private int age = 28;
 
+
+    /*MODIFICADORES DE ACESSO
+    - aumenta a segurança do codigo, possibilitando o uso de validações
+    - precisam ser publicos, para poder acessar os metodos externamente
+    - modificar -> set
+    - retornar -> get
+    */
+
+    //altera os valores
     public void setName(String name){
+        //o 'this' faz referecia ao atributo principal da classe -> so usar se repetir o nome das variaveis
         this.name = name;
     }
-    public void setAge(int age){
+    public void setAge(int age) {
         this.age = age;
     }
 
-    public String getName(){
+    //retorno dos valores
+    public String getName() {
         return name;
     }
-    public int getAge(){
+    public int getAge() {
         return age;
     }
 }

50c836663fb57cdd7141ac012b08a6668a56a578
Vinicius Coretti Tegi <viniciustegi1235@gmail.com>
2025-10-11 15:42:09 -0300

    Aula006 - last commit

--- Cursos/Bootcamp Riachuelo/Aulas/Aula006/src/Main.java
+++ Cursos/Bootcamp Riachuelo/Aulas/Aula006/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
+        var person1 = new Person("Jhon",23);
+        System.out.println(person1);
+        System.out.println(person1.name());
+        System.out.println(person1.age());
     }
 }
--- Cursos/Bootcamp Riachuelo/Aulas/Aula006/src/Person.java
+++ Cursos/Bootcamp Riachuelo/Aulas/Aula006/src/Person.java
@@ -1,2 +1,20 @@
-public record Person() {
+public record Person(String name, int age) {
+    /*
+    Para que serve o record?
+        Serve para leitura de dados, visto que uma vez criado, os objetos dentro deles serão imutaveis
+     */
+
+    //COMPACT CONSTRUCTOR
+    //vai rodar antes do restante do 'record'
+    public Person{
+        System.out.println("============");
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println("============end");
+    }
+
+    //CONSTRUTOR SECUNDARIO
+    public Person(String name){
+        this(name, 1);
+    }
 }

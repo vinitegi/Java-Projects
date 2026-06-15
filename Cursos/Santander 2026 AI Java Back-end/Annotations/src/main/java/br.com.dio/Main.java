f7b20651161def0badb14215463b85a787d14c05
Vinicius Coretti Tegi <viniciustegi1235@gmail.com>
2026-06-15 11:40:33 -0300

    Annotations - second commit

--- Cursos/Santander 2026 AI Java Back-end/Annotations/src/main/java/br.com.dio/Main.java
+++ Cursos/Santander 2026 AI Java Back-end/Annotations/src/main/java/br.com.dio/Main.java
@@ -1,7 +1,15 @@
 package br.com.dio;
 
-public class Main {
-    public static void main(String[] args) {
+import br.com.dio.model.Person;
+import br.com.dio.model.User;
+import br.com.dio.processor.SerializerProcessor;
 
+import java.lang.reflect.InvocationTargetException;
+
+public class Main {
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
+        var processor = new SerializerProcessor();
+        System.out.println(processor.serialize(new Person(1, "Jhon Silva", 26)));
+        System.out.println(processor.serialize(new User(2, "Maria Costa", 30, 3222.23)));
     }
 }
---
+++ Cursos/Santander 2026 AI Java Back-end/Annotations/src/main/java/br.com.dio/processor/SerializerProcessor.java
@@ -0,0 +1,72 @@
+package br.com.dio.processor;
+
+import br.com.dio.annotation.SerializerMethod;
+import br.com.dio.annotation.SerializerType;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.joining;
+
+public class SerializerProcessor {
+    public String serialize(final Object object) throws IllegalAccessException, InvocationTargetException {
+        Objects.requireNonNull(object, "Enter with non-null object");
+
+        var clazz = object.getClass();
+        var typeAnnotation = Stream.of(clazz.getAnnotations())
+                .flatMap(a -> (a instanceof SerializerType s) ? Stream.of(s) : Stream.empty())
+                /*.filter(SerializerType.class::isInstance)
+                .map(SerializerType.class::cast)*/
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("For serialize object annotate it with @SerializerType"));
+
+        var fieldNameFormatter = typeAnnotation.fieldFormat().getFormat();
+        var prettify = typeAnnotation.prettify();
+
+        Map<String, Object> elements = new HashMap<>();
+        for (var field : clazz.getDeclaredFields()) {
+            field.setAccessible(true);
+            elements.put(field.getName(), field.get(object));
+        }
+
+        var annotatedMethods = Stream.of(object.getClass().getMethods())
+                .filter(m -> Stream.of(m.getAnnotations()).anyMatch(a -> a.annotationType().equals(SerializerType.class)))
+                .toList();
+
+        for (var method : annotatedMethods) {
+            method.setAccessible(true);
+            var customName = method.getAnnotation(SerializerMethod.class).value();
+            elements.put(customName.isBlank() ? method.getName() : customName, method.invoke(object));
+        }
+
+        var jsonFields = elements.entrySet().stream()
+                .map(e -> String.format(
+                        "    \"%s\":%s",
+                        fieldNameFormatter.apply(e.getKey()),
+                        formatValue(e.getValue())
+                ))
+                .collect(joining(String.format(",%s", System.lineSeparator())));
+
+        var json = String.format("{%s%s%s}",
+                System.lineSeparator(),
+                jsonFields,
+                System.lineSeparator()
+        );
+
+        return prettify ?
+                json :
+                json.replaceAll(System.lineSeparator(), "")
+                .replaceAll(" ", "");
+
+    }
+
+    private String formatValue(final Object value) {
+        return value instanceof String s ?
+                String.format("\"%s\"", s) : value.toString();
+    }
+
+}
